7cb8b95686b6ed8e924a49a3b7f6e7086caef46b
Diogo Naka <diogo.naka@invillia.com>
2020-01-06 16:35:39 -0300

    First commit - Configuracao basica

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={} />
+    </Switch>
+  );
+}
