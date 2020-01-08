11c1da8a79ad248857e4154c80fa34cb57606011
Rafael Ornelas <rafael.ornelas@invillia.com>
2020-01-08 16:05:23 -0300

    create the category icons

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Historic from './pages/Historic';
+import WhereToUse from './pages/About/whereToUse/index';
 import About from './pages/About';
 import Quotation from './pages/Quotation';
+
 
 export default function Routes() {
   return (
     <Switch>
       <Route path={["/", "/historic"]} exact component={Historic} />
+      <Route path="/about/whereToUse" exact component={WhereToUse} />
       <Route path="/about" component={About} />
       <Route path="/quotation" component={Quotation} />
     </Switch>
