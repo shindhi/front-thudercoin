2901fef21e3ede22307d55536ee859e0948699d2
Tiago Salla <tiago.salla@hotmail.com>
2020-01-08 13:54:17 -0300

    first commit

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
+import History from './pages/History';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <Route path="/" exact component={History} />
     </Switch>
   );
 }
