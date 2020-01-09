7a07244e3b6b1458a73266aa3e6912476a5f64bd
Diogo Naka <diogo.naka@invillia.com>
2020-01-09 12:17:51 -0300

    Correcao - routes & About

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -8,7 +8,7 @@ export default function About() {
   return (
     <>
       <div className="links">
-        <Link to="/">Como funciona | </Link>
+        <Link to="/about/where-to-use">Onde usar| </Link>
         <Link to="/about/how-to-use">Como usar | </Link>
         <Link to="/about/how-to-earn">Como ganhar</Link>
       </div>
