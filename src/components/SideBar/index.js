0a4bf375106f8637adad8384ef1ad02a8ce245c9
Diogo Naka <diogo.naka@invillia.com>
2020-01-09 17:20:40 -0300

    Implementacao - Mudanca do fundo do botao de acordo com a pagina selecionada

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-
-
+import React, { useState, useEffect } from 'react';
 import { AiOutlinePlus, AiOutlineLineChart } from 'react-icons/ai';
 import { MdHistory, MdHelpOutline } from 'react-icons/md';
 
+import Mititititier from '../../assets/img/retarda.png';
 import {
   Bar,
   Container,
@@ -12,20 +11,37 @@ import {
   ConfigProfile,
   Menu,
   Item,
-  CoinThunder
+  CoinThunder,
 } from './styles';
 
-import Mititititier from '../../assets/img/retarda.png';
-
 export default function SideBar() {
+  const [item, setItem] = useState(null);
+
+  useEffect(() => {
+    const { pathname } = window.location;
+
+    if (pathname.substr(1) !== '') {
+      setItem(pathname.substr(1));
+    } else {
+      setItem('historic');
+    }
+  }, []);
+
+  // state = {
+  //   selectedItem: null,
+  // }
+
+  // handleXpto = (e) => {
+  //   this.setState({selectedItem: e.target.name})
+  // }
+
+  // <div classNames="ad" name="historico" onClick={handleXpto} isActive={selectedItem === 'historico'}></div>
+
   return (
     <Bar>
       <Container>
         <Profile>
-          <img
-            src={Mititititier}
-            alt="Avatar"
-          />
+          <img src={Mititititier} alt="Avatar" />
           <span>Mititititititititititier</span>
         </Profile>
 
@@ -45,17 +61,29 @@ export default function SideBar() {
         </Information>
 
         <Menu>
-          <Item to="/historic" >
+          <Item
+            to="/historic"
+            active={item === 'historic' && true}
+            onClick={() => setItem('historic')}
+          >
             <MdHistory size={50} color="#fff" />
             <span>Histórico</span>
           </Item>
 
-          <Item to="/about" >
+          <Item
+            to="/about"
+            active={item === 'about' && true}
+            onClick={() => setItem('about')}
+          >
             <MdHelpOutline size={50} color="#fff" />
             <span>Como funciona</span>
           </Item>
 
-          <Item to="/quotation" >
+          <Item
+            to="/quotation"
+            active={item === 'quotation' && true}
+            onClick={() => setItem('quotation')}
+          >
             <AiOutlineLineChart size={50} color="#fff" />
             <span>Cotação</span>
           </Item>
