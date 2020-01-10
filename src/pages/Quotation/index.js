7c9d31410ae1fae38207ea88c6c92ac82c47fcc8
Diogo Naka <diogo.naka@invillia.com>
2020-01-10 15:28:26 -0300

    enhancer - teste de graficos

--- src/pages/Quotation/index.js
+++ src/pages/Quotation/index.js
@@ -1,80 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import Chart from 'react-google-charts';
 
-import React, { useState, useEffect } from "react";
-import Chart from "react-google-charts";
-
-import {  Container,  QuotationTitle,  QuotationDesc, QuotationFilterBox,
-         QuotationByWeek, QuotationByWeekText , QuotationByWeekSelectButton
-          } from './styles';
+import axios from '../../utils/httpClient';
+import {
+  Container,
+  QuotationTitle,
+  QuotationDesc,
+  QuotationFilterBox,
+  QuotationByWeek,
+  QuotationByWeekText,
+  QuotationByWeekSelectButton,
+} from './styles';
 
 const columns = [
   {
-    type: "string",
-    label: "Mês"
+    type: 'string',
+    label: 'Mês',
   },
   {
-    label: "Cotação",
-    type: "number"
-  }
+    label: 'Cotação',
+    type: 'number',
+  },
 ];
 
 export default function Quotation() {
   const [rows, setRows] = useState([]);
 
-  useEffect(() => {
-    fetch("https://thundercoin-api.herokuapp.com/quotations")
-    .then(res => res.json())
-    .then(res => {
-      const data = res.map(r => ( [ r.createdAt.split(" ")[0], r.value ] ));
-      setRows(data)
-    })
+  useEffect(async () => {
+    const { data } = await axios.get('/quotations');
+
+    setRows(data.map(d => [d.createdAt.split(' ')[0], d.value]));
   }, []);
 
-  function handleStartDate(e) {
-    console.log(e.target.value);
-  }
-
-
-// ARRUMAR FUNCOES DO INPUT
+  // ARRUMAR FUNCOES DO INPUT
 
   return (
-   <Container>
-    <QuotationTitle>
-    <h1>Cotação ThunderCoin</h1></QuotationTitle>
+    <Container>
+      <QuotationTitle>
+        <h1>Cotação ThunderCoin</h1>
+      </QuotationTitle>
 
-    <QuotationFilterBox>
-
+      <QuotationFilterBox>
         <QuotationByWeek>
-          <QuotationByWeekText><h1>Data Inicial:</h1></QuotationByWeekText>
+          <QuotationByWeekText>
+            <h1>Data Inicial:</h1>
+          </QuotationByWeekText>
           <QuotationByWeekSelectButton>
-            <input type="date" onChange={handleStartDate} />
+            <input type="date" />
           </QuotationByWeekSelectButton>
-          <QuotationByWeekText><h1>Data Final:</h1></QuotationByWeekText>
+          <QuotationByWeekText>
+            <h1>Data Final:</h1>
+          </QuotationByWeekText>
           <QuotationByWeekSelectButton>
             <input type="date" label="Week" value="" />
           </QuotationByWeekSelectButton>
         </QuotationByWeek>
+      </QuotationFilterBox>
 
-    </QuotationFilterBox>
+      <div className="QuotationGraph">
+        <Chart
+          options={{
+            legendTextStyle: { color: '#FFF' },
+            titleTextStyle: { color: '#FFF' },
+            colors: ['#640096'],
+            backgroundColor: '#',
+          }}
+          chartType="AreaChart"
+          width="100vh"
+          height="50vh"
+          legendToggle
+          rows={rows}
+          columns={columns}
+        />
+      </div>
 
-    <div className="QuotationGraph">
-      <Chart
-       options={{legendTextStyle: { color: '#FFF' },
-                 titleTextStyle: { color: '#FFF' },
-                 colors: ['#640096'], backgroundColor: '#FE7600'}
-                }
-       chartType="AreaChart"
-       width="100vh"
-       height="50vh"
-       legendToggle
-       rows={rows}
-       columns={columns}
-      />
-    </div>
-
-    <QuotationDesc>
-      <h1>Acompanhe a cotação do ThunderCoin.</h1>
-    </QuotationDesc>
-
-  </Container>
+      <QuotationDesc>
+        <h1>Acompanhe a cotação do ThunderCoin.</h1>
+      </QuotationDesc>
+    </Container>
   );
 }
