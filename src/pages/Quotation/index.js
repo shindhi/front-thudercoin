5c76bfd6ce860eddae637800d2a99bd368e24693
RafaelCruz25 <rahrulez@hotmail.com>
2020-01-10 17:35:13 -0300

    quotation period fitler chart

--- src/pages/Quotation/index.js
+++ src/pages/Quotation/index.js
@@ -6,9 +6,6 @@ import {
   QuotationTitle,
   QuotationDesc,
   QuotationFilterBox,
-  QuotationByWeek,
-  QuotationByWeekText,
-  QuotationByWeekSelectButton,
 } from './styles';
 
 const columns = [
@@ -34,9 +31,7 @@ export default function Quotation() {
       });
   }, []);
 
-  function handleStartDate(e) {
-    console.log(e.target.value);
-  }
+  console.log(rows);
 
   return (
     <Container>
@@ -45,39 +40,71 @@ export default function Quotation() {
       </QuotationTitle>
 
       <QuotationFilterBox>
-        <QuotationByWeek>
-          <QuotationByWeekText>
-            <h1>Data Inicial:</h1>
-          </QuotationByWeekText>
-          <QuotationByWeekSelectButton>
-            <input type="date" onChange={handleStartDate} />
-          </QuotationByWeekSelectButton>
-          <QuotationByWeekText>
-            <h1>Data Final:</h1>
-          </QuotationByWeekText>
-          <QuotationByWeekSelectButton>
-            <input type="date" label="Week" value="" />
-          </QuotationByWeekSelectButton>
-        </QuotationByWeek>
+        <h1> CONSULTE POR PERÍODO.</h1>
       </QuotationFilterBox>
 
-      <div className="QuotationGraph">
-        <Chart
-          options={{
+      <Chart
+        width="100%"
+        chartType="AreaChart"
+        loader={<div>Loading Chart</div>}
+        data={[
+          ['Date', 'Value'],
+          [new Date(1997, 1, 1), 2000 * Math.random()],
+          [new Date(1998, 1, 1), 2000 * Math.random()],
+          [new Date(1999, 1, 1), 2000 * Math.random()],
+          [new Date(2000, 1, 1), 2000 * Math.random()],
+          [new Date(2001, 1, 1), 2000 * Math.random()],
+          [new Date(2002, 1, 1), 2000 * Math.random()],
+          [new Date(2003, 1, 1), 2000 * Math.random()],
+          [new Date(2004, 1, 1), 2000 * Math.random()],
+          [new Date(2005, 1, 1), 2000 * Math.random()],
+          [new Date(2006, 1, 1), 2000 * Math.random()],
+          [new Date(2007, 1, 1), 2000 * Math.random()],
+          [new Date(2008, 1, 1), 2000 * Math.random()],
+          [new Date(2009, 1, 1), 2000 * Math.random()],
+        ]}
+        options={{
+          legendTextStyle: { color: '#FFF' },
+          titleTextStyle: { color: '#FFF' },
+          colors: ['#640096'],
+          backgroundColor: 'rgb(255,255,0)',
+          // Use the same chart area width as the control for axis alignment.
+          chartArea: { height: '80%', width: '100%' },
+          hAxis: { slantedText: false },
+          vAxis: { viewWindow: { min: 0, max: 2000 } },
+          legend: { position: 'none' },
+        }}
+        rootProps={{ 'data-testid': '3' }}
+        chartPackages={['corechart', 'controls']}
+        controls={[
+          {
             legendTextStyle: { color: '#FFF' },
             titleTextStyle: { color: '#FFF' },
-            colors: ['#640096'],
-            backgroundColor: '#FE7600',
-          }}
-          chartType="AreaChart"
-          width="100vh"
-          height="50vh"
-          legendToggle
-          rows={rows}
-          columns={columns}
-        />
-      </div>
-
+            controlType: 'ChartRangeFilter',
+            options: {
+              filterColumnIndex: 0,
+              ui: {
+                chartType: 'LineChart',
+                chartOptions: {
+                  colors: ['red'],
+                  backgroundColor: 'rgb(255,255,0)',
+                  chartArea: { width: '70%', height: '60%' },
+                  hAxis: { baselineColor: 'none' },
+                },
+              },
+            },
+            controlPosition: 'top',
+            controlWrapperParams: {
+              state: {
+                range: {
+                  start: new Date(1997, 1, 1),
+                  end: new Date(2009, 1, 1),
+                },
+              },
+            },
+          },
+        ]}
+      />
       <QuotationDesc>
         <h1>Acompanhe a cotação do ThunderCoin.</h1>
       </QuotationDesc>
