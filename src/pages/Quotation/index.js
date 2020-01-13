93ae770076bac03991799cb7ec3d5ee8dfd94a91
RafaelCruz25 <rahrulez@hotmail.com>
2020-01-13 13:42:56 -0300

    quotation period initial/end

--- src/pages/Quotation/index.js
+++ src/pages/Quotation/index.js
@@ -8,31 +8,29 @@ import {
   QuotationFilterBox,
 } from './styles';
 
-const columns = [
-  {
-    type: 'string',
-    label: 'Mês',
-  },
-  {
-    label: 'Cotação',
-    type: 'number',
-  },
-];
-
 export default function Quotation() {
   const [rows, setRows] = useState([]);
+  const [initialDate, setInitialDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
 
   useEffect(() => {
     fetch('https://thundercoin-api.herokuapp.com/quotations')
       .then(res => res.json())
       .then(res => {
-        const data = res.map(r => [r.createdAt.split(' ')[0], r.value]);
+        const data = res.map(r => {
+          return { date: r.createdAt.split(' ')[0], value: r.value };
+        });
         setRows(data);
+        setInitialDate(data[0].date);
+        // console.log('initial date', initialDate, 'must be null');
+        // console.log('final date', endDate, 'must be null');
+        setEndDate(data[4].date);
       });
   }, []);
 
-  console.log(rows);
-
+  // console.log('rows', rows);
+  // console.log(rows);
+  // console.log(endDate);
   return (
     <Container>
       <QuotationTitle>
@@ -63,6 +61,22 @@ export default function Quotation() {
           [new Date(2008, 1, 1), 2000 * Math.random()],
           [new Date(2009, 1, 1), 2000 * Math.random()],
         ]}
+        // data={[
+        //   ['Date', 'Value'],
+        //   [rows[4].date, rows[4].value
+        //   [new Date(1998, 1, 1), 2000 * Math.random()],
+        //   [new Date(1999, 1, 1), 2000 * Math.random()],
+        //   [new Date(2000, 1, 1), 2000 * Math.random()],
+        //   [new Date(2001, 1, 1), 2000 * Math.random()],
+        //   [new Date(2002, 1, 1), 2000 * Math.random()],
+        //   [new Date(2003, 1, 1), 2000 * Math.random()],
+        //   [new Date(2004, 1, 1), 2000 * Math.random()],
+        //   [new Date(2005, 1, 1), 2000 * Math.random()],
+        //   [new Date(2006, 1, 1), 2000 * Math.random()],
+        //   [new Date(2007, 1, 1), 2000 * Math.random()],
+        //   [new Date(2008, 1, 1), 2000 * Math.random()],
+        //   [new Date(2009, 1, 1), 2000 * Math.random()],
+        // ]}
         options={{
           legendTextStyle: { color: '#FFF' },
           titleTextStyle: { color: '#FFF' },
@@ -97,7 +111,7 @@ export default function Quotation() {
             controlWrapperParams: {
               state: {
                 range: {
-                  start: new Date(1997, 1, 1),
+                  start: new Date(2009, 1, 1),
                   end: new Date(2009, 1, 1),
                 },
               },
@@ -105,6 +119,7 @@ export default function Quotation() {
           },
         ]}
       />
+
       <QuotationDesc>
         <h1>Acompanhe a cotação do ThunderCoin.</h1>
       </QuotationDesc>
