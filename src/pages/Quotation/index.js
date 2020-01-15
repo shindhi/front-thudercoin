2ab89df6f9ff591747ba3fe5ec682a4d75a83eca
Diogo Naka <diogo.naka@invillia.com>
2020-01-15 14:29:19 -0300

    alteracao na lista de datas caso venham vazias

--- src/pages/Quotation/index.js
+++ src/pages/Quotation/index.js
@@ -27,23 +27,36 @@ export default function Quotation() {
         'Novembro',
         'Dezembro',
       ];
-      const arrDate = [['Date', 'Value']];
+      let arrDate = [['Date', 'Value']];
 
       const { data } = await axios.get('/quotations');
 
-      data.map(d => {
-        const { createdAt, value } = d;
-        let convertDate = createdAt.split(' ')[0].split(/\//);
+      const arrTemp = [
+        ['Date', 'Value'],
+        [new Date(2020, 0, 9), 3.48],
+        [new Date(2020, 1, 10), 5.98],
+        [new Date(2020, 2, 11), 1.5],
+        [new Date(2020, 3, 5), 2.5],
+        [new Date(2020, 4, 20), 0.7],
+      ];
 
-        convertDate = [convertDate[1], convertDate[0], convertDate[2]].join(
-          '/'
-        );
+      if (data === '') {
+        arrDate = arrTemp;
+      } else {
+        data.map(d => {
+          const { createdAt, value } = d;
+          let convertDate = createdAt.split(' ')[0].split(/\//);
+          convertDate = [convertDate[1], convertDate[0], convertDate[2]].join(
+            '/'
+          );
 
-        return arrDate.push([new Date(convertDate), value]);
+          return arrDate.push([new Date(convertDate), value]);
 
-        // const monthPosition = new Date(convertDate).getMonth();
-        // return arrDate.push([months[monthPosition], value]);
-      });
+          // const monthPosition = new Date(convertDate).getMonth();
+          // return arrDate.push([months[monthPosition], value]);
+        });
+      }
+
       setDates(arrDate);
     }
 
@@ -68,15 +81,14 @@ export default function Quotation() {
             width: '80%',
             height: '70%',
             backgroundColor: '#fff',
-            borderRadius: '50%',
           }, // Area interna grafico
           hAxis: {
             title: 'Data',
             titleTextStyle: { color: '#000' },
             format: 'd/M/yy',
-            gridlines: { count: 15 },
+            // gridlines: { count: 15 },
           }, // Legenda rodape grafico
-          vAxis: { title: 'Valores (R$)', minValue: 0, format: 'currency' },
+          vAxis: { title: 'Valores (R$)', format: 'currency' },
         }}
       />
     </Container>
